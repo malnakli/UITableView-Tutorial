4c92a9694c64c794215ecd018422341a89e4bc22
malnakli <hassoun@outlook.com>
2015-09-09 16:53:05 -0400

    setup UITableView

---
+++ UITableView-Tutorial/Candy.swift
@@ -0,0 +1,14 @@
+//
+//  Candy.swift
+//  UITableView-Tutorial
+//
+//  Created by MoAir on 2015-09-09.
+//  Copyright (c) 2015 Mo. All rights reserved.
+//
+
+import UIKit
+
+struct Candy {
+    let category : String
+    let name : String
+}
---
+++ UITableView-Tutorial/CandyTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CandyTableViewController.swift
+//  UITableView-Tutorial
+//
+//  Created by MoAir on 2015-09-09.
+//  Copyright (c) 2015 Mo. All rights reserved.
+//
+
+import UIKit
+
+class CandyTableViewController: UITableViewController {
+
+    var candies = [Candy]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Sample Data for candyArray
+        self.candies = [Candy(category:"Chocolate", name:"chocolate Bar"),
+            Candy(category:"Chocolate", name:"chocolate Chip"),
+            Candy(category:"Chocolate", name:"dark chocolate"),
+            Candy(category:"Hard", name:"lollipop"),
+            Candy(category:"Hard", name:"candy cane"),
+            Candy(category:"Hard", name:"jaw breaker"),
+            Candy(category:"Other", name:"caramel"),
+            Candy(category:"Other", name:"sour chew"),
+            Candy(category:"Other", name:"gummi bear")]
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.candies.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //ask for a reusable cell from the tableview, the tableview will create a new one if it doesn't have any
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        // Get the corresponding candy from our candies array
+        let candy = self.candies[indexPath.row]
+        
+        // Configure the cell
+        cell.textLabel!.text = candy.name
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
