82bc64b1c7fa4a2ad87ffbf3a7e3d2cbdcb55214
malnakli <hassoun@outlook.com>
2015-09-09 17:58:05 -0400

    setup search bar

--- UITableView-Tutorial/CandyTableViewController.swift
+++ UITableView-Tutorial/CandyTableViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class CandyTableViewController: UITableViewController {
+// UISearchBarDelegate defines the behavior and response of a search
+// UISearchDisplayDelegate defines the look and feel of the search bar.
+class CandyTableViewController: UITableViewController , UISearchBarDelegate, UISearchDisplayDelegate{
 
     var candies = [Candy]()
-
+    var filteredCandies = [Candy]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,6 +28,8 @@ class CandyTableViewController: UITableViewController {
             Candy(category:"Other", name:"caramel"),
             Candy(category:"Other", name:"sour chew"),
             Candy(category:"Other", name:"gummi bear")]
+        // Reload the table
+        self.tableView.reloadData()
         // Do any additional setup after loading the view.
     }
 
@@ -34,15 +39,23 @@ class CandyTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.candies.count
-    }
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            return self.filteredCandies.count
+        } else {
+            return self.candies.count
+        }    }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         //ask for a reusable cell from the tableview, the tableview will create a new one if it doesn't have any
-        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
         
-        // Get the corresponding candy from our candies array
-        let candy = self.candies[indexPath.row]
+        var candy : Candy
+        // Check to see whether the normal table or search results table is being displayed and set the Candy object from the appropriate array
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            candy = filteredCandies[indexPath.row]
+        } else {
+            candy = candies[indexPath.row]
+        }
         
         // Configure the cell
         cell.textLabel!.text = candy.name
@@ -50,6 +63,32 @@ class CandyTableViewController: UITableViewController {
         
         return cell
     }
+    
+    
+    //  filter()  {(parameters) -> (return type) in expression statements}
+    // rangeOfString() checks if a string contains another string. If it does and the category also matches, you return true and the current candy is included in the filtered array; if it doesn’t you return false and the current candy is not included.
+    func filterContentForSearchText(searchText: String) {
+        // Filter the array using the filter method
+        self.filteredCandies = self.candies.filter({( candy: Candy) -> Bool in
+          //  let categoryMatch = (scope == "All") || (candy.category == scope)
+            let stringMatch = candy.name.rangeOfString(searchText)
+            //return categoryMatch && (stringMatch != nil)
+            return (stringMatch != nil)
+
+            
+        })
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchString searchString: String!) -> Bool {
+        self.filterContentForSearchText(searchString)
+        return true
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchScope searchOption: Int) -> Bool {
+        self.filterContentForSearchText(self.searchDisplayController!.searchBar.text)
+        return true
+    }
+    
     /*
     // MARK: - Navigation
 
